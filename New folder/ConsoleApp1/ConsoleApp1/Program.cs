9af31aa6f380a9b6f77bc5d55f68415ce70aae30
nrug13 <nurgunshg@code.edu.az>
2022-03-24 12:20:19 +0400

    ok

---
+++ New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 reqemli eded verilib. Bu ededin eveline 7 reqemini
+
+            //, axirinada 8 reqemini artir
+
+
+
+            Console.WriteLine("eded daxil edin:");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            string s = Convert.ToString(a);
+
+            if (s.Length != 4)
+
+
+
+            {
+
+                Console.WriteLine("xeta");
+
+            }
+
+            else
+
+            {
+
+                int b = 8;
+
+                int d = 7;
+
+
+
+                int newNumber = int.Parse(d.ToString() + a.ToString() + b.ToString());
+
+                Console.WriteLine(newNumber);
+
+
+
+            }
+        }
+    }
+}
