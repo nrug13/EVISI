b739f91475f6097182200aae041e3aacf6e61847
nrug13 <nurgunshg@code.edu.az>
2022-03-24 12:32:34 +0400

    task 4

--- New folder/ConsoleApp1/ConsoleApp1/Program.cs
+++ New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //4 reqemli eded verilib. Bu ededin eveline 7 reqemini
-
-            //, axirinada 8 reqemini artir
-
-
-
             Console.WriteLine("eded daxil edin:");
 
             int a = Convert.ToInt32(Console.ReadLine());
 
             string s = Convert.ToString(a);
 
-            if (s.Length != 4)
+            if (s.Length != 3)
 
 
 
@@ -32,15 +26,19 @@ namespace ConsoleApp1
 
             {
 
-                int b = 8;
-
-                int d = 7;
+                int b = 7;
 
 
 
-                int newNumber = int.Parse(d.ToString() + a.ToString() + b.ToString());
+                int newNumber = int.Parse(a.ToString() + b.ToString());
 
                 Console.WriteLine(newNumber);
+
+                double x = newNumber * 7 / 100;
+
+                Console.WriteLine(x);
+
+                //3 reqemli eded verilib. Bu ededin axirina 7 artir. Sonra cavabin 7% tap;
 
 
 
